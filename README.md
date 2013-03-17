69b49e3ad7350c29c7d4644fa35dde230b472cc6
William Myers <griffin.myers@gmail.com>
2013-03-17 00:43:08 -0400

    Initial Commit... rails skeleton

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-jaught
-======
+journal
+=======
+
+This is a little web app, just for me, so I can publish journal entires, if I
+want.
+
+It uses some some Latex parsers I wrote to make things look nice.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Journal::Application.config.secret_token = '1d75761f7b187ab51a3ff83b65740974cb416aa70adef3f22ab66b2744be7d0f7edf0bca2dd9c5e36986b1a9f3568f5deb961d1858bb7a89dee817b70e1e1801'
