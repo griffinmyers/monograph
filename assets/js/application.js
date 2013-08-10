64fcddeed6782af0fc999489ec1dcd049e2468a3
William Myers <griffin.myers@gmail.com>
2013-08-09 20:03:26 -0400

    Bumping hemingway version and template changes

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,4 +1,4 @@
-$(document).on("click", "a", function(){
+$("article").on("click", "a", function(){
   $('html, body').animate({
     scrollTop: $( $.attr(this, 'href') ).offset().top - 30
   }, 500);
